afb37c37947d0009be12fbda2620a4276384f9de
BrayaMDev <cg4463380@gmail.com>
2026-05-20 23:15:11 -0600

    feat: agrego Ticket

---
+++ src/main/java/com/axity/dinosaurpark/model/Ticket.java
@@ -0,0 +1,47 @@
+package com.axity.dinosaurpark.model;
+
+import java.time.LocalDateTime;
+
+public class Ticket {
+
+    private final long id;
+    private final int touristId;
+    private final double price;
+    private final String category;
+    private final LocalDateTime issuedAt;
+
+    public Ticket(long id, int touristId, double price, String category) {
+        this.id = id;
+        this.touristId = touristId;
+        this.price = price;
+        this.category = category;
+        this.issuedAt = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public LocalDateTime getIssuedAt() {
+        return issuedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", touristId=" + touristId +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", issuedAt=" + issuedAt +
+                '}';
+    }
+}
