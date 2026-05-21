51e27c9b44a6bf2aeb50b066e1c673e6eecb6973
BrayaMDev <cg4463380@gmail.com>
2026-05-20 22:44:52 -0600

    feat: agrego Guard

---
+++ src/main/java/com/axity/dinosaurpark/model/Guard.java
@@ -0,0 +1,23 @@
+package com.axity.dinosaurpark.model;
+
+import java.util.List;
+
+public class Guard extends Worker {
+
+    public Guard(int id, String name, double dailySalary) {
+        super(id, name, dailySalary);
+    }
+
+    @Override
+    public String getRole() {
+        return "GUARD";
+    }
+
+    public void recaptureEscapedDinosaurs(List<Dinosaur> dinosaurs) {
+        for (Dinosaur d : dinosaurs) {         // recorre cada dinosaurio
+            if (d.getStatus() == DinosaurStatus.ESCAPED) {  // Comprueba si escapo
+                d.returnToEnclosure();          // Aqui si es recapturado
+            }
+        }
+    }
+}
