397841775da4bb6c7865738afcea7fab2a67ff39
BrayaMDev <cg4463380@gmail.com>
2026-05-20 20:20:41 -0600

    feat: agrego DinosaurStatus y Dinosaur clase abstracta

---
+++ src/main/java/com/axity/dinosaurpark/model/DinosaurStatus.java
@@ -0,0 +1,4 @@
+package com.axity.dinosaurpark.model;
+
+//Los estado del dinosaurio: Encerrado, Escapo, Recapturado
+public enum DinosaurStatus { IN_ENCLOSURE, ESCAPED, RECAPTURED }
--- src/main/java/com/axity/dinosaurpark/model/TouristStatus.java
+++ src/main/java/com/axity/dinosaurpark/model/TouristStatus.java
@@ -1,3 +1,4 @@
 package com.axity.dinosaurpark.model;
 
+//Aqui se presentan los estado del turista, En espera o comprado boletos, en el parque, atacado y fuera del parque
 public enum TouristStatus { WAITING, IN_PARK, ATTACKED, EXITED }
