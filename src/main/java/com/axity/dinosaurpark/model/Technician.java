651a0bab398fbff7e7739d4069badc449897cd07
BrayaMDev <cg4463380@gmail.com>
2026-05-20 23:08:29 -0600

    feat: agrego Technician

---
+++ src/main/java/com/axity/dinosaurpark/model/Technician.java
@@ -0,0 +1,20 @@
+package com.axity.dinosaurpark.model;
+
+public class Technician extends Worker {
+
+    public Technician(int id, String name, double dailySalary) {
+        super(id, name, dailySalary);
+    }
+
+
+    @Override
+    public String getRole() {
+        return "TECHNICIAN";
+    }
+
+    public void repairIfNeeded(PowerPlant plant) {  // Planta de energia
+        if (!plant.isOperational()) {  // Pregunta si la planta esta funcionado
+            plant.repair(); //  En caso de no funcionar se tiene que reparar
+        }
+    }
+}
