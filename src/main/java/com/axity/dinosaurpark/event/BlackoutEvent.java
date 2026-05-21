0a0e56a6757c3df38dddbd22cdd556c011038111
BrayaMDev <cg4463380@gmail.com>
2026-05-21 16:49:00 -0600

    feat: agrego BlackoutEvent

---
+++ src/main/java/com/axity/dinosaurpark/event/BlackoutEvent.java
@@ -0,0 +1,31 @@
+package com.axity.dinosaurpark.event;
+
+import com.axity.dinosaurpark.zone.PowerPlant;
+
+public class BlackoutEvent implements ParkEvent {
+
+    private final PowerPlant powerPlant;
+    private String description;
+    private double cost;
+
+    public BlackoutEvent(PowerPlant powerPlant) {
+        this.powerPlant = powerPlant;
+        this.description = "";
+        this.cost = 0.0;
+    }
+
+    @Override
+    public String getType() { return "BLACKOUT"; }
+
+    @Override
+    public String getDescription() { return description; }
+
+    public double getCost() { return cost; }
+
+    @Override
+    public void execute() {
+        cost = powerPlant.triggerFailure();
+        description = "Costo de la reparación: " + cost;
+        powerPlant.repair();
+    }
+}
