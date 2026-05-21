fd5de58f25271452957bd27edd78f482eb2cba17
BrayaMDev <cg4463380@gmail.com>
2026-05-21 00:25:41 -0600

    feat: agrego ExperienceType enum

---
+++ src/main/java/com/axity/dinosaurpark/zone/ExperienceType.java
@@ -0,0 +1,7 @@
+package com.axity.dinosaurpark.zone;
+
+public enum ExperienceType {
+    BASIC,    // puntuación del 1 al 3
+    PREMIUM,  // puntuación del 2 al 4
+    VIP       // puntuación del 3 al 5
+}
